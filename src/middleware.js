fe5a4a09c6ec5c110adc383809304f7959576f72
Rasel Rana <rasel.learn22@gmail.com>
2023-09-07 02:30:40 +0600

    the middleware function implement

--- src/middleware.js
+++ src/middleware.js
@@ -2,27 +2,21 @@ import { NextResponse } from "next/server";
 import { verifyTokenCookie } from "./app/lib/verify-token-cookie";
 
 export async function middleware(request) {
-    // if (request.nextUrl.pathname.startsWith('/')) {
-    //     console.log('root path');
-    //     return NextResponse.next();
-    // }
-    // if (request.nextUrl.pathname.startsWith('/login')) {
-    //     console.log('login path');
-    //     return NextResponse.next();
-    // }
-    if (request.nextUrl.pathname.startsWith('/home')) {
-        try {
-            // Verify the token cookie
-            const verified = await verifyTokenCookie(request);
+    try {
+        // Verify the token cookie
+        const verified = await verifyTokenCookie(request);
 
-            if (verified.ok) {
-                return NextResponse.next();
-            } else {
-                return NextResponse.redirect(new URL('/login', request.url))
-            }
-
-        } catch (error) {
-            console.log(error);
+        if (verified.ok) {
+            return NextResponse.next();
+        } else {
+            return NextResponse.redirect(new URL('/login', request.url))
         }
+
+    } catch (error) {
+        console.log(error);
     }
+}
+
+export const config = {
+    matcher: ['/home/:path*', '/profile/:path*', '/contact/:path*', '/about/:path*', '/logout/:path*'],
 }
