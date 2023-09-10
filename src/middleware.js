d6220a564f5d1614c95f0a650b6e46fc05a03503
Rasel Rana <rasel.learn22@gmail.com>
2023-09-10 23:29:03 +0600

    middleware test

--- src/middleware.js
+++ src/middleware.js
@@ -2,26 +2,26 @@ import { NextResponse } from "next/server";
 import { verifyTokenCookie } from "./app/lib/verify-token-cookie";
 
 export async function middleware(request) {
-    try {
-        // Verify the token cookie
-        // add the header in verifyTokenCookie function
-        const verified = await verifyTokenCookie(request);
+    //try {
+    // Verify the token cookie
+    // add the header in verifyTokenCookie function
+    //     const verified = await verifyTokenCookie(request);
 
-        if (verified.ok) {
-            return NextResponse.next();
-        } else {
-            return NextResponse.redirect(new URL('/login', request.url))
-        }
+    //     if (verified.ok) {
+    //         return NextResponse.next();
+    //     } else {
+    //         return NextResponse.redirect(new URL('/login', request.url))
+    //     }
 
-    } catch (error) {
-        console.log(error);
-    }
+    // } catch (error) {
+    //     console.log(error);
+    // }
 
     /* When I attempted to use a try and catch block to check the 
      destination path and verified that it was working correctly locally,
      I encountered issues when deploying to Vercel.As a result,
     I had to comment out this code.*/
-    // return NextResponse.next();
+    return NextResponse.next();
 }
 
 // The destination path after login
